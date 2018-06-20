Havoc-OS/android_vendor_havoc

Add support for building custom themes

products/common.mk
@@ -108,3 +108,5 @@ PRODUCT_BOOT_JARS += \
 # DU Utils Library
 PRODUCT_PACKAGES += \
     org.dirtyunicorns.utils
+# Themes
+$(call inherit-product-if-exists, vendor/havoc/products/themes.mk)

jansvanda/android_vendor_mnml

products/common.mk
@@ -79,3 +79,6 @@ PRODUCT_BOOT_JARS += \
 # DU Utils Library
 PRODUCT_PACKAGES += \
     org.dirtyunicorns.utils
+
+# Themes
+$(call inherit-product-if-exists, vendor/mnml/products/themes.mk)