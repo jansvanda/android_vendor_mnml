nitrogen-project/android_vendor_nitrogen

Add apn-conf.xml

products/common.mk
@@ -54,6 +54,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/nitrogen/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
+# APN
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Extra packages
 PRODUCT_PACKAGES += \
     DaylightHeaderNitrogen \

jansvanda/android_vendor_mnml

products/common.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 
+# APN
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Extra packages
 PRODUCT_PACKAGES += \
     OmniJaws \